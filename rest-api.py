eb213c48cff8281a33c791baf129c02bfe7804d5
Miroslav Dobsicek <dobsicek@chalmers.se>
2020-04-21 20:32:58 +0200

    Job upload, WIP

--- rest-api.py
+++ rest-api.py
@@ -25,11 +25,18 @@ from pydantic import BaseModel
 import Labber
 import shutil
 import pathlib
+from uuid import uuid4
 
 # .env configuration
 config = Config(".env")
 NAME = config("NAME", default="NO-NAME")
 DB_URL = config("DB_URL", default="NO-DB-URL")
+PREFIX = config("PREFIX", default="pingu")
+API_PREFIX = config("API_PREFIX", default=PREFIX)
+STORAGE_PREFIX = config("STORAGE_PREFIX", default=PREFIX)
+STORAGE_LOCATION = config("STORAGE_LOCATION", default="/tmp")
+UPLOAD_POOL_DIRNAME = config("UPLOAD_POOL_DIRNAME", default="upload_pool")
+
 
 # mongodb
 mongodb = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
@@ -40,12 +47,29 @@ collection = db["t1_mon"]
 logfile = pathlib.Path("/tmp/logfile.hdf5")
 
 # redis connection
-redis_conn = Redis()
+redis_connection = Redis()
 # redis queue
-q_high = Queue("high", connection=redis_conn)
-q_mid = Queue("mid", connection=redis_conn)
-q_low = Queue("low", connection=redis_conn)
+q_high = Queue("high", connection=redis_connection)
+q_mid = Queue("mid", connection=redis_connection)
+q_low = Queue("low", connection=redis_connection)
 
+rq_job_preprocessing = Queue(PREFIX + "_job_preprocessing", connection=redis_connection)
+
+# pydantic models
+class Item(BaseModel):
+    description: str = None
+    id: int
+
+
+class Job(BaseModel):
+    type: str
+    name: str
+    params: dict = None
+    hdf5_log_extraction: dict = None
+    hdf5_log_retention: str = None
+
+
+# labber worker task
 labber = LabberFrontend()
 
 # application
@@ -65,6 +89,26 @@ async def root():
 
     # output
     return {"message": "Hello from " + NAME + ", T1: " + str(cursor["value"])}
+
+
+@app.post("/jobs")
+async def upload_job(upload_file: UploadFile = File(...)):
+
+    # generate unique file name
+    uuid = uuid4()
+    file_name = str(uuid)
+    file_path = pathlib.Path(STORAGE_LOCATION) / STORAGE_PREFIX / UPLOAD_POOL_DIRNAME
+    file_path.mkdir(parents=True, exist_ok=True)
+    store_file = file_path / file_name
+
+    # save it
+    with store_file.open("wb") as destination:
+        shutil.copyfileobj(upload_file.file, destination)
+    upload_file.file.close()
+
+    # enqueue for processing
+    rq_job_preprocessing.enqueue(labber.iJob, str(store_file), "1")
+    return {"message": file_name}
 
 
 @app.post("/logfiles")
@@ -102,11 +146,6 @@ async def root_q():
     return {"message": "Job id: " + job.id + " created"}
 
 
-class Item(BaseModel):
-    description: str = None
-    id: int
-
-
 @app.post("/rec")
 async def record(item: Item):
     return {"message": "Received id: " + str(item.id)}
@@ -114,7 +153,7 @@ async def record(item: Item):
 
 @app.get("/rq-info")
 async def get_rq_info():
-    workers = Worker.all(connection=redis_conn)
+    workers = Worker.all(connection=redis_connection)
     print(str(workers))
     if workers == []:
         return {"message": "No worker registered"}
