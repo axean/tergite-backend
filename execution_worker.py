7961bf546625b14ca9131dbb2e90d58271d3383b
Miroslav Dobsicek <dobsicek@chalmers.se>
2020-05-07 15:34:51 +0200

    Connect execution worker and Labber rest-api

--- execution_worker.py
+++ execution_worker.py
@@ -10,18 +10,22 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-import pathlib
+from pathlib import Path
 import json
 import time
 from scenario_scripts import demodulation_scenario
 from uuid import uuid4
+import requests
 
 
-def execute_job(file):
-    print(f"Executing file {str(file)}")
+def execute_job(job_file: Path):
+    print(f"Executing file {str(job_file)}")
 
     job_dict = {}
-    with file.open() as f:
+    scenario_id = uuid4()
+    scenario_file = Path(r"/tmp") / (str(scenario_id) + ".labber")
+
+    with job_file.open() as f:
         job_dict = json.load(f)
 
     if job_dict["name"] == "demodulation_scenario":
@@ -30,11 +34,25 @@ def execute_job(file):
 
         scenario = demodulation_scenario(signal_array, demod_array)
 
-        scenario.log_name = "Test signal demodulation - " + str(uuid4())
-        scenario.save("/tmp/my.json", save_as_json=True)
-        print("Scenario generated at /tmp/my.json")
+        scenario.log_name = "Test signal demodulation - " + str(scenario_id)
+        # scenario.save("/tmp/my.json", save_as_json=True)
+        scenario.save(scenario_file)
+        print(f"Scenario generated at {str(scenario_file)}")
     else:
         print(f"Unknown script name {job_dict['name']}")
 
-    # clean up
-    file.unlink()
+    with scenario_file.open("rb") as source:
+        files = {"upload_file": source}
+        url = "http://mc2-p045.mc2.chalmers.se:5000/scenarios"
+        response = requests.post(url, files=files)
+
+    if response:
+        # clean up
+        job_file.unlink()
+        scenario_file.unlink()
+
+        print("OK")
+        return {"message": "ok"}
+    else:
+        print("Failed")
+        return {"message": "failed"}
---
+++ start_bcc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# TODO:
+# - convert this script to a supervisor deployment
+# - paths and names should come from a configuration file
+
+# path (mostly for uvicorn; --app-dir option will come in a new release)
+cd /home/dobsicek/repos/tergite-bcc
+
+# clean start
+rq empty pingu_job_preprocessing pingu_job_execution
+rm -fr /tmp/pingu
+
+# worker processes
+rq worker --path /home/dobsicek/repos/tergite-bcc pingu_job_preprocessing &
+rq worker --path /home/dobsicek/repos/tergite-bcc pingu_job_execution &
+
+# rest-api
+uvicorn --host 0.0.0.0 --port 5000 rest-api:app --reload
