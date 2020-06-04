50bbecfcb6f6aa4b9f60aef561931eb874a9c19e
Miroslav Dobsicek <dobsicek@chalmers.se>
2020-06-04 18:01:48 +0200

    Push Qiskit/Qobj result to MSS

--- postprocessing_worker.py
+++ postprocessing_worker.py
@@ -3,6 +3,7 @@ import time
 from uuid import uuid4
 from starlette.config import Config
 import Labber
+import requests
 
 # config
 config = Config(".env")
@@ -19,16 +20,24 @@ JOB_EXECUTION_POOL_DIRNAME = config(
 LOGFILE_DOWNLOAD_POOL_DIRNAME = config(
     "LOGFILE_DOWNLOAD_POOL_DIRNAME", default="logfile_download_pool"
 )
+MSS_MACHINE_ROOT_URL = config(
+    "MSS_MACHINE_ROOT_URL", default="http://qdp-git.mc2.chalmers.se:5000"
+)
+
+REST_API_MAP = {
+    "result": "/result",
+    "status": "/status",
+    "timelog": "/timelog",
+    "jobs": "/jobs",
+}
 
 
 def logfile_postprocess(logfile: Path):
 
     print(f"Postprocessing logfile {str(logfile)}")
 
-    # mimick logfile post-processing
-    time.sleep(2)
-
-    new_file_name = str(uuid4())
+    # move logfile to download area
+    new_file_name = str(uuid4()) + ".hdf5"
     storage_location = Path(STORAGE_ROOT) / STORAGE_PREFIX_DIRNAME
 
     new_file_path = storage_location / LOGFILE_DOWNLOAD_POOL_DIRNAME
@@ -39,8 +48,44 @@ def logfile_postprocess(logfile: Path):
 
     print(f"Created new file {str(new_file)}")
 
+    ###############################################################
+    # The below works/makes sense only for Qiskit/QObj jobs.
+    # Currently no check for this is in place !!!!
+    #   - one WA possibility is to use tags exactly as with job_id
+    #   - proper solution is a job supervisor
+    ###############################################################
+
+    # extract System state
+    memory = extract_system_state_as_hex(new_file)
+    print(memory)
+
+    # extract job_id
+    job_id = extract_job_id(new_file)
+    URL = MSS_MACHINE_ROOT_URL + REST_API_MAP["jobs"] + "/" + job_id
+
+    # NOTE: When MSS adds support for the 'whole job' update
+    # this will just one PUT request
+    response = requests.put(URL + REST_API_MAP["result"], json=memory)
+    if response:
+        print("Pushed result to MSS")
+
+    response = requests.post(URL + REST_API_MAP["timelog"], json="RESULT")
+    if response:
+        print("Updated job timelog on BCC")
+
+    response = requests.put(URL + REST_API_MAP["status"], json="DONE")
+    if response:
+        print("Updated job status on MSS to DONE")
+
 
 def extract_system_state_as_hex(logfile: Path):
     f = Labber.LogFile(logfile)
     raw_data = f.getData("State Discriminator - System state")
     return [hex(int(x)) for x in raw_data[0]]
+
+
+def extract_job_id(logfile: Path):
+    f = Labber.LogFile(logfile)
+    job_id = f.getTags()[0]
+    print(job_id)
+    return job_id
