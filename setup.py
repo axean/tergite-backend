53b93069790838356e13946aed6c7496a1a0c80e
Miroslav Dobsicek <dobsicek@chalmers.se>
2021-05-28 14:58:35 +0200

    Step to Python 3.8
    > Description
    -------------
    Tergite BCC has been verified to work with Python 3.8. Minimal
    versions of dependent packages have been stepped inline with this
    verification.
    
    The setup.py triggers installation of requirements. The BCC
    is expected to be installed in a development mode with
    
        pip install -e .
    
    exactly as Tergite MSS.
    
    > Verification
    --------------
    Functionality has been verified with the whole stack:
    Qiskit, MSS, BCC, Labber.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # This code is part of Tergite
 #
-# (C) Copyright Miroslav Dobsicek 2020
+# (C) Copyright Miroslav Dobsicek 2020, 2021
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -14,17 +14,17 @@
 from setuptools import setup, find_packages
 
 REQUIREMENTS = [
-    "aiofiles>=0.5.0",
-    "fastapi>=0.61.0",
-    "motor>=2.2.0",
+    "aiofiles>=0.7.0",
+    "fastapi>=0.65.1",
+    "motor>=2.4.0",
     "python-multipart>=0.0.5",
     "redis>=3.5.3",
-    "requests>=2.24.0",
-    "rq>=1.5.0",
-    "uvicorn>=0.11.8",
-    "numpy>=1.19.1",
-    "PyQt5>=5.14.2",
-    "h5py>=2.10.0",
+    "requests>=2.25.1",
+    "rq>=1.8.1",
+    "uvicorn>=0.13.4",
+    "numpy>=1.20.3",
+    "PyQt5>=5.15.4",
+    "h5py>=3.2.1",
     "scipy>=1.4.1",
     "networkx>=2.5.1",
 ]
@@ -35,5 +35,5 @@ setup(
     license="Apache 2.0",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
