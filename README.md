c4e6e8505772803541afbd501b7f65758deb5adb
Miroslav Dobsicek <dobsicek@chalmers.se>
2021-10-18 15:54:27 +0200

    [BC-38] Use port number from .env file

---
+++ README.md
@@ -0,0 +1,14 @@
+# Installation
+
+## Prerequisites
+* Redis
+* Labber, + export PYTHONPATH to Labber's Script module
+* (optional) Use Anaconda environment. With Python 3.8.
+
+## Package installation
+* cd tergite-bcc
+* pip install -e .
+* create .env file with BCC configuration. See dot-env-template.txt.
+
+## Start
+* ./start_bcc.sh
--- start_bcc.sh
+++ start_bcc.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+exit_with_error () {
+    echo "Port configuration failed. Use BCC_PORT=<num> in the .env file."
+    exit 1
+}
+
+# port handling
+PORT_CONFIG=$(grep BCC_PORT= .env)               # eg: BCC_PORT=5000
+PORT_NUMBER="${PORT_CONFIG#*=}"                  # extract the number
+[[ -z "$PORT_NUMBER" ]]  &&  exit_with_error     # validation
+[[ ! "$PORT_NUMBER" =~ ^[0-9]+$ ]]  &&  exit_with_error
+
 
 # TODO:
-# - convert this script to a supervisor deployment
-# - paths and names should come from a configuration file
+# - clean up fixed paths from this script
+# - they don't do any harm at the moment but they are confusing
+
 
 # path (mostly for uvicorn; --app-dir option will come in a new release)
 cd /home/dobsicek/repos/tergite-bcc
@@ -17,4 +30,4 @@ rq worker --path /home/dobsicek/repos/tergite-bcc pingu_job_execution &
 rq worker --path /home/dobsicek/repos/tergite-bcc pingu_logfile_postprocessing &
 
 # rest-api
-uvicorn --host 0.0.0.0 --port 5000 rest_api:app --reload
+uvicorn --host 0.0.0.0 --port "$PORT_NUMBER" rest_api:app --reload
