e9daa1cd6de80a18c08b23de0f13c9a77f51da20
Fabian Forslund <fabfors@student.chalmers.se>
2022-02-10 16:03:01 +0100

    Public job handling methods added
    Co-authored-by: Nicklas Botö <git@nicbot.xyz>

--- job_supervisor.py
+++ job_supervisor.py
@@ -16,12 +16,12 @@ import time
 import shutil
 from pathlib import Path
 import settings
+from typing import Dict
 import redis
 
 STORAGE_ROOT = settings.STORAGE_ROOT
 LABBER_MACHINE_ROOT_URL = settings.LABBER_MACHINE_ROOT_URL
 BCC_MACHINE_ROOT_URL = settings.BCC_MACHINE_ROOT_URL
-CALIBRATION_SUPERVISOR_PORT = settings.CALIBRATION_SUPERVISOR_PORT
 JOB_SUPERVISOR_LOG = settings.JOB_SUPERVISOR_LOG
 STORAGE_PREFIX_DIRNAME = settings.STORAGE_PREFIX_DIRNAME
 
@@ -38,6 +38,16 @@ LOCALHOST = "localhost"
 
 red = redis.Redis()
 
+# TODO: Add job supervisor query 
+async def query_redis(query: str) -> str:
+    result = await red.hget("job_supervisor", query)
+    return result
+
+
+async def fetch_redis_entry(job_id: str) -> Dict[str, str]:
+    """Query redis for job supervisor entry"""
+    key = f"JS_{job_id}"
+    return red.hget(key)
 
 @unique
 class Location(Enum):
@@ -52,46 +62,93 @@ class Location(Enum):
     FINAL_Q    = 8
     FINAL_W    = 9
 
-# data Location
-#   = REG
-#   | PRE_PROC
-#   | EXEC
-#   | PST_PROV
-#   deriving Enum
+def register_job(job_id: str) -> None:
+    """"Format job file for storage"""
+    entry_id = f"JS_{job_id}"
+
+    entry = {
+        "id": entry_id,
+        "priorities": {
+            "global": 0,
+            "local": {
+                "pre_processing": 0,
+                "execution": 0,
+                "post_processing": 0
+            }
+        },
+        "status": {
+            "location": Location.REG_W,
+            "started": time.now(),
+            "finished": None,
+            "cancelled": {
+                "time": None,
+                "reason": None
+            }
+        },
+        "results": None
+    }
+    red.hset("job_supervisor", entry_id, entry)
+
+    # log entry
+    log(f"Registered entry for job {job_id}")
 
 
-def register(job: Path) -> None:
-    """Add job entry to redis"""
+def update_job_entry(job_id: str, value, *keys) -> None:
+    entry = query_redis(job_id)
+    deep_update(entry, value, keys)
+    red.hset("job_supervisor", job_id, entry)
+
+
+def deep_update(dict, value, keys):
+    for i, key in enumerate(keys):
+        if key == keys[-1]:
+            dict[key] = value
+        else:
+            sub = deep_update(dict[key], keys[i:], value)
+            dict[key] = sub
+    return dict
+
+
+def cancel_job(job_id: str, reason: str) -> None:
     pass
 
-# TODO: Add job supervisor query 
-async def query_redis(query: str) -> str:
-    result = await red.hget(query)
-    pass
+def inform_results(job_id: str, results) -> None:
+    """Upload results to redis"""
+
+    update_job_entry(job_id, time.now(), "status", "finished")
+    update_job_entry(job_id, results, "results")
+
+    log("Job {job_id} finished with results")
+
+
+async def inform_location(job_id: str, location: Location) -> None:
+    """"Update job location"""
     
+    parse_log: Dict[Location, str] = {
+        Location.REG_Q      : "registration queue",
+        Location.REG_W      : "registration worker",
+        Location.PRE_PROC_Q : "pre-processing queue",
+        Location.PRE_PROC_W : "pre-processing worker",
+        Location.EXEC_Q     : "execution queue",
+        Location.EXEC_W     : "execution worker",
+        Location.PST_PROC_Q : "post-processing queue",
+        Location.PST_PROC_W : "post-processing worker",
+        Location.FINAL_Q    : "finalization queue",
+        Location.FINAL_W    : "finalization worker"    
+    }
 
-# NOTE: This message handling is WIP! The calibration loop does *not* depend on it.
-# Current status: The messages are ariving, but very late.
-async def handle_message(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info("peername")
+    # update job position in redis
+    job_entry = red.hget(job_id)
+    job_entry["status"]["location"] = location
+    red.hset(job_id, job_entry)
 
-    print(f"Received {message!r} from {addr!r}")
-    writer.close()
-
-async def message_server():
-    server = await asyncio.start_server(
-        handle_message, LOCALHOST, CALIBRATION_SUPERVISOR_PORT)
-    async with server:
-        await server.serve_forever()
+    # log updated job position
+    log(f"{job_id} arrived at {parse_log[location]}")
 
 
 async def main():
-    server_task = asyncio.create_task(message_server())
-    calib_task = asyncio.create_task(check_calib_status())
+    server_task = asyncio.create_task()
     await server_task
-    await calib_task
 
 
 @unique
@@ -104,7 +161,13 @@ class LogLevel(Enum):
 def log(message: str, level: LogLevel = LogLevel.INFO) -> None:
     """Save message to job supervisor log file"""
 
-    logstring = f"[{time.now()}] {level.name}: {message}"
+    color = (
+        '\033[0m', # color end
+        '\033[0;33m', # yellow
+        '\033[0;31m' # red
+    )
+
+    logstring = f"{color[level.value]}[{time.now()}] {level.name}: {message}{color[0]}"
  
     file_path = Path(STORAGE_ROOT) / STORAGE_PREFIX_DIRNAME 
     file_path.mkdir(parents=True, exist_ok=True)
