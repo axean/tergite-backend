300ba09d913d3cd5aa58da9d7a6b07ccdc60eb24
Stefan Hill <stefanhi@chalmers.se>
2023-09-13 14:32:31 +0200

    Fix: Change default worker names from pingu to loki

--- start_bcc.sh
+++ start_bcc.sh
@@ -28,8 +28,8 @@ PORT_NUMBER="${PORT_CONFIG#*=}"                  # extract the number
 
 
 # Clean start
-rq empty pingu_job_registration pingu_job_preprocessing pingu_job_execution pingu_logfile_postprocessing
-rm -fr /tmp/pingu    # FIXME: Fixed path
+rq empty loki_job_registration loki_job_preprocessing loki_job_execution loki_logfile_postprocessing
+rm -fr /tmp/loki    # FIXME: Fixed path
 
 
 # Remove old Redis keys, by their prefixes
@@ -58,10 +58,10 @@ if test -n "$unrecognized_args"; then
 fi
 
 # Worker processes
-rq worker pingu_job_registration &
-rq worker pingu_job_preprocessing &
-rq worker pingu_job_execution &
-rq worker pingu_logfile_postprocessing &
+rq worker loki_job_registration &
+rq worker loki_job_preprocessing &
+rq worker loki_job_execution &
+rq worker loki_logfile_postprocessing &
 
 # REST-API
 uvicorn --host 0.0.0.0 --port "$PORT_NUMBER" rest_api:app --reload
